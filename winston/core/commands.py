10436d19ca61edb7c3b933fc756184025d13268b
James <monkhousejames@gmail.com>
2026-07-24 01:02:34 +0100

    Added command registry and first Winston skills

--- winston/core/commands.py
+++ winston/core/commands.py
@@ -3,13 +3,19 @@ from datetime import datetime
 
 class CommandHandler:
 
+    def __init__(self):
+        self.commands = {
+            "help": self.help,
+            "time": self.time,
+            "hello": self.hello,
+            "weather": self.weather,
+            "date": self.date,
+        }
+
     def execute(self, command):
 
-        if command == "help":
-            return self.help()
-
-        if command == "time":
-            return self.time()
+        if command in self.commands:
+            return self.commands[command]()
 
         return "I'm afraid I haven't learned that skill yet, sir."
 
@@ -19,6 +25,18 @@ class CommandHandler:
         current_time = current_datetime.strftime("%H:%M")
 
         return f"The current time is {current_time}, sir."
+
+    def hello(self):
+        return "Hello, sir. How may I assist you today?"
+
+    def weather(self):
+        return "I'm sorry, sir. I don't have access to real-time weather information."
+
+    def date(self):
+        current_datetime = datetime.now()
+        current_date = current_datetime.strftime("%Y-%m-%d")
+
+        return f"The current date is {current_date}, sir."
 
     def help(self):
         return """
